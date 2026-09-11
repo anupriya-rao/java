066150c0f8dff568f80193966ce61b9d11ed5483
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 19:15:29 +0530

    string

---
+++ strings1.java
@@ -0,0 +1,8 @@
+public class strings1 {
+    public static void main(String[] args) {
+        String name = "pranshu";
+        int value = name.length();
+        System.out.println(value);
+
+    }
+}
