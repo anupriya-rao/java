f4e89f6b990dbd7807c174383f82cb6ec6294432
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 18:14:30 +0530

    ques 2

---
+++ ques2.java
@@ -0,0 +1,11 @@
+public class ques2 {
+    public static void main(String[] args) {
+        char i = 'A';
+        i = (char)(i+8) ;
+        System.out.println(i);
+        // decoding 
+        i = (char)(i-8);
+        System.out.println(i);
+
+    }
+}
