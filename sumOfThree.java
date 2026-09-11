2ac41204e65e70c24a9504205f5d3c71a6320fbd
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 17:00:45 +0530

    update project

---
+++ sumOfThree.java
@@ -0,0 +1,21 @@
+public class sumOfThree {
+    public static void main(String[] srgs){
+       //ques 1 
+       // int a = 4;
+       //int b= 7;
+        //int c = 9;
+        //int sum = a + b + c;
+        //System.out.println(sum);
+        
+        //ques 2 
+        // cgpa using marks of 3 subject 
+        int a = 99;
+        int b=34;
+        int c = 95;
+        int sum = a+b+c;
+        int cgpa = sum/3;
+        System.out.println(cgpa);
+
+
+    }
+}
