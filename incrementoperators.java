8ba172f18d993f45c171083db4630d0802e17d9d
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 17:54:09 +0530

    increment

---
+++ incrementoperators.java
@@ -0,0 +1,8 @@
+public class incrementoperators {
+    public static void main(String[] args) {
+        int a = 78;
+        float b = 8.44f ;
+        float c = a+b ;
+        System.out.println(c);
+    }
+}
