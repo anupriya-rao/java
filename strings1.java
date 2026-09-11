db98eb63b761ad9747c404b3d05f123277d4f6ec
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 19:22:29 +0530

    ss

--- strings1.java
+++ strings1.java
@@ -7,6 +7,8 @@ public class strings1 {
         System.out.println(lstring);
         String l1string = name.toUpperCase();
         System.out.println(l1string);
+        String nonTrim = "    Pranshu       ";
+        System.out.println(nonTrim.trim());
 
     }
 }
