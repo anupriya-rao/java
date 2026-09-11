d2e00d8226e3ca5c4d97336c58a39e57c8a86006
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 18:31:47 +0530

    ques5

---
+++ ques4.java
@@ -0,0 +1,11 @@
+public class ques4 {
+    public static void main(String[] args) {
+        int v = 3;
+        int u = 2;
+        int a = 1;
+        int s = 2;
+        float solve = (float)(v*v - u*u)/(2*a*s);
+        System.out.println(solve);
+
+    }
+}
---
+++ ques5.java
@@ -0,0 +1,7 @@
+public class ques5 {
+    public static void main(String[] args) {
+        int x = 7;
+        float a = 7*49/7+35/7;
+        System.out.println(a);
+    }
+}
