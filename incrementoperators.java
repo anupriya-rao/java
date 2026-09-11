c1d97141a102e6cb81f7a5aa7f7462a80718552d
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 17:56:53 +0530

    i+=

--- incrementoperators.java
+++ incrementoperators.java
@@ -1,8 +1,11 @@
 public class incrementoperators {
     public static void main(String[] args) {
-        int a = 78;
+        /*int a = 78;
         float b = 8.44f ;
         float c = a+b ;
-        System.out.println(c);
+        System.out.println(c);*/
+
+        int i = 78;
+        System.out.println(i++);
     }
 }
