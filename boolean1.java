cd2eacbe631e8b413d0d1bc6b8ab1db6a07b7ef7
anupriya-rao <anupriyarao75@gmail.com>
2026-09-11 20:02:34 +0530

    ss

---
+++ boolean1.java
@@ -0,0 +1,12 @@
+public class boolean1 {
+   public static void main(String[] args){
+    boolean a = true;
+    boolean b = true;
+    if(a&&b){
+        System.out.println("y");
+    }
+    else{
+        System.out.println("x");
+    }
+   }
+}
