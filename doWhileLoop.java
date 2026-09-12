8b415852918036c5e16fc9a9d38717276ba48a25
anupriya-rao <anupriyarao75@gmail.com>
2026-09-12 22:32:57 +0530

    ss

--- doWhileLoop.java
+++ doWhileLoop.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class doWhileLoop {
     public static void main(String[] args) {
-    int a = 1;
+    // program to print first n natural nums using do - while loops 
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int i = 1;
     do{
-        System.out.println(a);
-        a++;
-    } while(a<10);
+        System.out.println(i);
+        i++;
+    }while (i <= n);
+
     }
 }
