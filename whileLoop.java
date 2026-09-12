fa52272d1e059993e9fc1e93e5bc4034197b98ec
anupriya-rao <anupriyarao75@gmail.com>
2026-09-12 22:19:07 +0530

    sk

--- whileLoop.java
+++ whileLoop.java
@@ -1,9 +1,11 @@
 public class whileLoop {
     public static void main(String[] args) {
-        int i = 1;
-        while(i<=5){
+        // print numbers from 100 to 200
+        int i = 100 ;
+        while(i<200){
             System.out.println(i);
             i++;
         }
+        System.out.println("loop has ended");
     }
 }
