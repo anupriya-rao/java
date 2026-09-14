50be6a0ad8b9e20ebc8f555ea77cb4f58f6406a4
anupriya-rao <anupriyarao75@gmail.com>
2026-09-14 22:23:14 +0530

    ss

---
+++ break_and_continue.java
@@ -0,0 +1,12 @@
+public class break_and_continue {
+    public static void main(String[] args) {
+        for(int i = 0 ; i<=10; i++){
+            System.out.println(i);
+            System.out.println("this is java");
+            if(i==2){
+                System.out.println("end loop");
+                break;
+            }
+        }
+    }
+}
