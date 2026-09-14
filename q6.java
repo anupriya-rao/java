56b26ef524ff907d33f64f1db4fecbf16e12f510
anupriya-rao <anupriyarao75@gmail.com>
2026-09-15 01:42:56 +0530

    7

---
+++ q6.java
@@ -0,0 +1,11 @@
+public class q6 {
+    public static void main(String[] args) {
+        int [] num = { 1 , 4 , 6, 7 ,8 ,3, 9};
+        int n = 7;
+        for(int i = 0; i<7 ; i++){
+            if(n == num[i]){
+            System.out.println("it is present");
+            }
+        }
+    }
+}
