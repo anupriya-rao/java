2ac60d61341b2d6aa3c1039d40c3162339a31194
anupriya-rao <anupriyarao75@gmail.com>
2026-09-15 00:31:31 +0530

    ss

--- array.java
+++ array.java
@@ -1,10 +1,12 @@
 public class array {
     public static void main(String[] args) {
-        int [] marks ;
+        /*int [] marks ;
         marks = new int[3];
         marks[0] = 78;
         marks[1]=34;
         marks[2]=99;
-        System.out.println(marks[2]);
+        System.out.println(marks[2]);*/
+        int [] marks = {33,56,89,23,57,56,78,45};
+        System.out.println(marks[3]);
     }
 }
