c77ec9ac2819a511ade348bae9f13f4642873e34
anupriya-rao <anupriyarao75@gmail.com>
2026-09-15 00:28:06 +0530

    aa

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        int [] marks ;
+        marks = new int[3];
+        marks[0] = 78;
+        marks[1]=34;
+        marks[2]=99;
+        System.out.println(marks[2]);
+    }
+}
