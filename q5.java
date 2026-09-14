2953f2b80a82b5a650b36b5f61144b11a5b717a4
anupriya-rao <anupriyarao75@gmail.com>
2026-09-15 01:38:17 +0530

    6

---
+++ q5.java
@@ -0,0 +1,12 @@
+public class q5 {
+    public static void main(String[] args) {
+        float [] num = { 1.2f , 3.4f , 7.3f , 6.4f , 2.3f};
+        float sum = 0;
+        System.out.println("sum of array is :");
+        for(int i = 0 ; i<5 ; i++){
+            sum = sum + num[i];
+        }
+        System.out.println(sum);
+        
+    }
+}
