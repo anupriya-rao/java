16305f74bd9c4d27286ef92620fdddaa4c97f75b
anupriya-rao <anupriyarao75@gmail.com>
2026-09-14 23:32:06 +0530

    aa

--- q3.java
+++ q3.java
@@ -1,9 +1,10 @@
 public class q3 {
     public static void main(String[] args) {
+        int n = 7;
 
         for(int i = 1; i <= 10; i++){
             int sum = 7*i;
-            System.out.println(sum);
+            System.out.println(n + "x" + i +"="+ sum);
         }
 
     }
