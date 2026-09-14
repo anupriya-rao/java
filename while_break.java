fb60faabd579369b0ff8c3f499a3207b6bf6e33e
anupriya-rao <anupriyarao75@gmail.com>
2026-09-14 22:28:59 +0530

    1

--- while_break.java
+++ while_break.java
@@ -1,12 +1,12 @@
 public class while_break {
     public static void main(String[] args) {
         int i = 1;
-        while(i<100){
+        while(i<10){
             System.out.println("this is java");
             System.out.println(i);
-            if(i==78){
+            if(i==3){
                 System.out.println("loop has ended");
-                break;
+                continue;
             }
             i++;
         }
