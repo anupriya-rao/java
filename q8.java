91333f619cb7650c44fd6751ad05cf8e039855c3
anupriya-rao <anupriyarao75@gmail.com>
2026-09-15 11:53:19 +0530

    11

--- q8.java
+++ q8.java
@@ -1,10 +1,13 @@
 public class q8 {
     public static void main(String[] args) {
-        // reverse an array
-        int [] num = { 1 , 3 , 4, 5, 6, 7,8 , 9 , 10};
-        for(int i = num.length-1 ; i>=0 ; i--){
-            System.out.print(" " + num[i]);
+        // min element
+       int [] num = { 1 , 3, 45 , 56, 234 , 4556};
+       int min = num[0];
+       for(int i = 0 ; i<num.length; i++){
+        if(num[i]<min){
+            min=num[i];
         }
-
+       }
+       System.out.println(min);
     }
 }
