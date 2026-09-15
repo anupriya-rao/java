3cf1c754a6573c81ec281afd0fc0523b8dee5fcb
anupriya-rao <anupriyarao75@gmail.com>
2026-09-15 11:42:51 +0530

    9

---
+++ q8.java
@@ -0,0 +1,10 @@
+public class q8 {
+    public static void main(String[] args) {
+        // reverse an array
+        int [] num = { 1 , 3 , 4, 5, 6, 7,8 , 9 , 10};
+        for(int i = num.length-1 ; i>=0 ; i--){
+            System.out.print(" " + num[i]);
+        }
+
+    }
+}
