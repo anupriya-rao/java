0c1316da280b697882860ef4dc4e0771145ee1f7
anupriya-rao <anupriyarao75@gmail.com>
2026-09-17 00:45:23 +0530

    20

--- varArgs.java
+++ varArgs.java
@@ -2,12 +2,12 @@ public class varArgs {
     static int sum(int ...arr){
         int result = 0;
         for(int a : arr){
-            result =+0;
+            result =+ a;
         }
         return result;
 
     }
     public static void main(String[] args) {
-        System.out.println("sum of 4,5 is" + sum(4,5));
+        System.out.println("sum of 4,5 is - " + sum(4,5));
     }
 }
