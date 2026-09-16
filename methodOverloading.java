9ae87616450dac9130a90350b2ca947869091cd9
anupriya-rao <anupriyarao75@gmail.com>
2026-09-17 00:08:08 +0530

    16

---
+++ methodOverloading.java
@@ -0,0 +1,11 @@
+public class methodOverloading {
+    
+        static void joke(){
+            System.out.println("ths is a girl");
+
+        }
+    public static void main(String[] args) {
+        joke();
+    }    
+}
+
