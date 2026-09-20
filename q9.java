7b610dac081af444bafaa418204de348184275b9
anupriya-rao <anupriyarao75@gmail.com>
2026-09-20 21:17:03 +0530

    14

---
+++ q9.java
@@ -0,0 +1,13 @@
+public class q9 {
+    static void multiplication(int n){
+        for(int i=1; i<11; i++){
+            System.out.format("%d X %d = %d\n" , n,i,n*i);
+        }
+
+    }
+    public static void main(String[] args) {
+        // java program to print a multiplication table of a number n
+        multiplication(7);
+
+    }
+}
