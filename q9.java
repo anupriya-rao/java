00da50ebf60ad3c95646eb744ae262e43036a8ab
anupriya-rao <anupriyarao75@gmail.com>
2026-09-22 22:11:15 +0530

    15

--- q9.java
+++ q9.java
@@ -1,9 +1,8 @@
 public class q9 {
     static void multiplication(int n){
-        for(int i=1; i<11; i++){
-            System.out.format("%d X %d = %d\n" , n,i,n*i);
+        for( int i = 1 ; i<=10 ; i++){
+            System.out.format("% d * %d = %d \n" , n , i , n*i);
         }
-
     }
     public static void main(String[] args) {
         // java program to print a multiplication table of a number n
