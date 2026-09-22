7f4540b1ed946eb1a04d934fc0355a9030ee0921
anupriya-rao <anupriyarao75@gmail.com>
2026-09-23 00:41:00 +0530

    17

---
+++ customClass.java
@@ -0,0 +1,13 @@
+class employee{
+    int id ; 
+    String name;
+}
+public class customClass {
+    public static void main(String[] args) {
+        employee pranshu = new employee();
+        pranshu.id = 11;
+        pranshu.name = "anupriya";
+        System.out.println(pranshu.id);
+
+    }
+}
